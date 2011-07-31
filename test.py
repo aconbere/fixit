4cfc55f78ae20b66541e6069c785aec046fb9c1e
Anders Conbere <aconbere@gmail.com>
2011-07-31 17:14:45 -0500

    showing some shortcuts

--- test.py
+++ test.py
@@ -44,6 +44,15 @@ roles.row("sd").set(name                 = "State Director",
                     default_boss_role_id = roles.nrd.id
                     )
 
+# This is a couple of the shortcuts available by design
+# .f means from (from is a stupid keyword in python so I can't use it)
+# it means inherit from a previous defined row or dict
+#
+# .set looks at *args as well as **kwargs and zips args with the column names
+# So if you know what order the columns are in, you can just hand args to it
+# that way. In this case this sets the Name of "fd" to Field Director
+roles.row("fd").f(roles.sd).set("Field Director")
+
 fixit.setup(db_session, roles)
 
 for role in db_session.query(Role).all():
